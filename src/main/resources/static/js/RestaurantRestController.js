32bb651b3bc5a9d9ff9208f1d9abe5cfc794ce39
Juan David Ramirez Mendoza <juan.ramirez-me@mail.escuelaing.edu.co>
2018-03-03 18:24:15 -0500

    Some methods added

--- src/main/resources/static/js/RestaurantRestController.js
+++ src/main/resources/static/js/RestaurantRestController.js
@@ -1,4 +1,5 @@
-var RestControllerModule = (function () {
+var RestControllerModule = (function () 
+{
 
   var getOrders = function (callback) {
     axios.get('/orders')
@@ -9,13 +10,35 @@ var RestControllerModule = (function () {
           callback.onFailed(reason);
         });
   };
+  
+  var getOrderById = function(orderId, callback){
+      axios.get('/orders/'+orderId)
+              .then(function (response){
+                  callback.onSuccess(response.data);
+              })
+              .catch(function (reason){
+                  callback.onFailed(reason);
+              });
+  }
 
   var updateOrder = function (order, callback) {
-    // todo implement
+    axios.put('/orders/'+actual,order)
+            .then(function(){
+                callback.onSuccess();
+            })
+            .catch(function(reason){
+                callback.onFailed(reason);
+            });
   };
 
   var deleteOrder = function (orderId, callback) {
-    // todo implement
+    axios.delete('/orders/'+orderId)
+            .then(function(){
+                callback.onSuccess();
+            })
+            .catch(function(reason){
+                callback.onFailed(reason);
+            });
   };
 
   var createOrder = function (order, callback) {
@@ -24,6 +47,7 @@ var RestControllerModule = (function () {
 
   return {
     getOrders: getOrders,
+    getOrderById: getOrderById,
     updateOrder: updateOrder,
     deleteOrder: deleteOrder,
     createOrder: createOrder
